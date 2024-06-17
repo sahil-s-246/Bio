a365ed4b0b47fb0ea8b81b78e858dfbaefc77928
sahil-s-246 <sahilsrinivas3@gmail.com>
2024-06-17 16:05:31 +0530

    Init

---
+++ main.py
@@ -0,0 +1,36 @@
+# This is a sample Python script.
+import streamlit as st
+import random
+import time
+
+
+st.title("Hallo, I'm Sahil Parupudi")
+
+"""
+A Computer Engineering Student at Dr. D.Y. Patil Institute of Technology,Pimpri.Trying to learn and explore
+field of data science and ML whilst being surrounded by trees in a random forest 
+"""
+col1, col2, col3,col4 = st.columns([1,1,1,1])
+
+with col1:
+    st.link_button("Github","https://github.com/sahil-s-246")
+with col2:
+    st.link_button("LinkedIn","https://linked.com/sahilparupudi")
+with col3:
+    st.link_button("Bio","https://github.com/sahil-s-246")
+with col4:
+    st.link_button("Resume","https://sahil-s-246.github.io/autoCV")
+
+"Experience:"
+{
+   "Rubiscape":["Jun-Sep 2025","Project Intern"]
+}
+"Technologies:"
+{
+    "Languages":["Python","Java","C++"],
+    "Web":["HTML","CSS","JS","Flask"],
+    "Libraries":["Pandas","Seaborn","Scikit-learn","BeautifulSoup"],
+    "Misc":["Git","Apps Script","MySQL","MongoDB"]
+}
+
+
